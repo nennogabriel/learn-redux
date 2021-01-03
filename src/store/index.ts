0d057ae4e0a55e8a018c23e0519956ba3fbfbd74
Pedro Moreno <nennogabriel@gmail.com>
2021-01-03 12:19:23 -0300

    refactoring catalog item

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,12 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
 
 export interface IState {
-  cart: ICartState
+  cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
